0776218f1d634396885f900192232eb74d2142ab
fajar4561 <maulanafajar751@gmail.com>
2021-10-09 21:15:16 +0000

    switch to linage Signed-off-by: fajar4561 <maulanafajar751@gmail.com>

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -19,17 +19,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common RR stuff
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from whyred device
+# Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := X00TD
 PRODUCT_MANUFACTURER := asus
-PRODUCT_NAME := rr_X00TD
-PRODUCT_MODEL := ASUS_X00TD
+PRODUCT_NAME := lineage_X00TD
+PRODUCT_MODEL := Zenfone Max Pro M1
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
@@ -45,8 +45,3 @@ BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/relea
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
-
-# RR Stuffs
-TARGET_FACE_UNLOCK_SUPPORTED := true
-RR_BUILDTYPE := Unofficial
-BUILD_RR_WALLPAPERS:= true
